583ea0c10d933c5c77c7e7c2541e13504f847ec8
Addison Crump <addison.crump@cispa.de>
2023-03-08 02:55:56 +0100

    start work with .bib nonsense

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fuzzing-summaries
-Summaries and relationship data for fuzzing papers
+
+Summaries and relationship data for fuzzing papers.
+
+## General Idea
+
+Exploring relationships between papers can be hard, and it's not always easy to
+find papers you know you've read and can no longer find. I have been struggling
+to keep track of fuzzing papers recently and figure others are too, so I decided
+to make this.
+
+This repository contains summaries, bibliographies, and referrer/referee data
+for as many fuzzing papers as I could read and summarise.
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use biblatex::Bibliography;
+use log::LevelFilter;
+use std::error::Error;
+use std::fs::{read_dir, read_to_string};
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::builder()
+        .filter_level(LevelFilter::Warn)
+        .parse_default_env()
+        .init();
+
+    let all = Bibliography::parse(&read_to_string("fuzzing-papers.bib")?)
+        .expect("Could not parse the bibliography!");
+
+    let summary_dir = PathBuf::from("summaries");
+    for entry in all.iter() {
+        let name = &entry.key;
+        if !summary_dir.join(name).exists() {
+            log::warn!("Missing summary for {}.", name);
+        }
+    }
+
+    Ok(())
+}
